2d594f2e2e0ee46cb16e43275ed6bb887bbaca76
Denis Trifan <trifan.denis1999@gmail.com>
2024-05-20 16:28:00 +0300

    added initial files

---
+++ src/test/java/voting/sys/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package voting.sys.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
