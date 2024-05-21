65a5d6119832a9358d03e2c18add1591773ba3c0
Denis Trifan <trifan.denis1999@gmail.com>
2024-05-21 14:28:17 +0300

    removed unnecessary print

--- src/main/java/voting/sys/webapp/api/security/CurrentToken.java
+++ src/main/java/voting/sys/webapp/api/security/CurrentToken.java
@@ -5,14 +5,12 @@ import lombok.Getter;
 public class CurrentToken {
 
     @Getter
-    private static String authToken;
+    private static String authToken = "";
 
     public static void setAuthToken(String token) {
         CurrentToken.authToken = token;
-        System.out.println("Auth token set to " + token);
     }
 
     private CurrentToken() {
-        authToken = "";
     }
 }
